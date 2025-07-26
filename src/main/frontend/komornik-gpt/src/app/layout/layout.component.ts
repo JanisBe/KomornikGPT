7b72dc8407c63ccdc452fb86053b9cfba60d9261
Janis Blatsios <janis666@gmail.com>
2025-07-26 23:41:49 +0200

    menu z grupami

--- src/main/frontend/komornik-gpt/src/app/layout/layout.component.ts
+++ src/main/frontend/komornik-gpt/src/app/layout/layout.component.ts
@@ -1,5 +1,4 @@
 import {Component, HostListener, OnInit, ViewChild} from '@angular/core';
-
 import {RouterModule} from '@angular/router';
 import {AuthService} from '../core/services/auth.service';
 import {User} from '../core/models/user.model';
@@ -8,11 +7,17 @@ import {MatIconModule} from "@angular/material/icon";
 import {MatToolbarModule} from "@angular/material/toolbar";
 import {MatSidenav, MatSidenavModule} from "@angular/material/sidenav";
 import {MatListModule} from "@angular/material/list";
+import {GroupService} from "../core/services/group.service";
+import {Group} from "../core/models/group.model";
+import {Observable, of} from "rxjs";
+import {MatMenuModule} from "@angular/material/menu";
+import {AsyncPipe} from "@angular/common";
+import {MatExpansionModule} from "@angular/material/expansion";
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [RouterModule, MatButtonModule, MatIconModule, MatToolbarModule, MatSidenavModule, MatListModule],
+  imports: [RouterModule, MatButtonModule, MatIconModule, MatToolbarModule, MatSidenavModule, MatListModule, MatMenuModule, AsyncPipe, MatExpansionModule],
   template: `
     <mat-toolbar color="primary">
       <button mat-icon-button class="menu-button" (click)="sidenav.toggle()">
@@ -24,7 +29,16 @@ import {MatListModule} from "@angular/material/list";
 
       <div class="desktop-nav">
         @if (authService.isAuthenticated()) {
-          <a mat-button routerLink="/groups" routerLinkActive="active">Grupy</a>
+          <a mat-button [matMenuTriggerFor]="groupsMenu" routerLinkActive="active">Grupy</a>
+          <mat-menu #groupsMenu="matMenu">
+            <a mat-menu-item routerLink="/groups">Wszystkie grupy</a>
+            @if ((myGroups$ | async)?.length) {
+              <hr>
+              @for (group of myGroups$ | async; track group.id) {
+                <a mat-menu-item [routerLink]="['/groups', group.id]">{{ group.name }}</a>
+              }
+            }
+          </mat-menu>
           <a mat-button routerLink="/expenses" routerLinkActive="active">Moje wydatki</a>
           <a mat-button routerLink="/profile" routerLinkActive="active">Profil ({{ userName }})</a>
           <a mat-button (click)="logout()">Logout</a>
@@ -45,7 +59,21 @@ import {MatListModule} from "@angular/material/list";
       <mat-sidenav #sidenav mode="over" [fixedInViewport]="true" [fixedTopGap]="64">
         <mat-nav-list>
           @if (authService.isAuthenticated()) {
-            <a mat-list-item routerLink="/groups" routerLinkActive="active" (click)="sidenav.close()">Grupy</a>
+            <mat-accordion>
+              <mat-expansion-panel class="mat-elevation-z0">
+                <mat-expansion-panel-header>Grupy</mat-expansion-panel-header>
+                <ng-template matExpansionPanelContent>
+                  <mat-nav-list>
+                    <a mat-list-item routerLink="/groups" routerLinkActive="active" (click)="sidenav.close()">Wszystkie
+                      grupy</a>
+                    @for (group of myGroups$ | async; track group.id) {
+                      <a mat-list-item [routerLink]="['/groups', group.id]"
+                         (click)="sidenav.close()">{{ group.name }}</a>
+                    }
+                  </mat-nav-list>
+                </ng-template>
+              </mat-expansion-panel>
+            </mat-accordion>
             <a mat-list-item routerLink="/expenses" routerLinkActive="active" (click)="sidenav.close()">Moje wydatki</a>
             <a mat-list-item routerLink="/profile" routerLinkActive="active" (click)="sidenav.close()">Profil
               ({{ userName }})</a>
@@ -111,6 +139,17 @@ import {MatListModule} from "@angular/material/list";
       display: none;
     }
 
+    .mat-expansion-panel-header {
+      padding: 0 16px;
+      font-family: var(--mdc-list-list-item-label-text-font);
+      font-size: var(--mdc-list-list-item-label-text-size);
+      font-weight: var(--mdc-list-list-item-label-text-weight);
+    }
+
+    .mat-expansion-panel .mat-nav-list a[mat-list-item] {
+      padding-left: 32px;
+    }
+
     @media (max-width: 768px) {
       .desktop-nav {
         display: none;
@@ -130,16 +169,22 @@ export class LayoutComponent implements OnInit {
   userName = '';
   deferredPrompt: any;
   showInstallButton = false;
+  myGroups$: Observable<Group[]> = of([]);
 
   @ViewChild('sidenav') sidenav!: MatSidenav;
 
-  constructor(public authService: AuthService) {
+  constructor(public authService: AuthService, private groupService: GroupService) {
   }
 
   ngOnInit(): void {
     this.authService.user$.subscribe({
       next: (user: User | null) => {
         this.userName = user?.name ?? '';
+        if (user) {
+          this.myGroups$ = this.groupService.getMyGroups();
+        } else {
+          this.myGroups$ = of([]);
+        }
       }
     });
   }
